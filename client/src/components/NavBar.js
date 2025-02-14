cea698b9c15079a407453a6fa27219fe049d01f2
JoshuaR132 <jrroberts141@gmail.com>
2025-02-14 14:42:04 +0000

    Added a feature to allow the user to switch between dark mode and light mode

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,30 @@
-// Import neccessary files
-import React from 'react';
+// Import necessary files
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import "../styles/main.css";
 import LoginForm from './LoginForm';
+import DarkModeToggle from './DarkModeToggle'; // Import Dark Mode Toggle
 
 const NavBar = () => {
-  const [isLoginFormVisible, setIsLoginFormVisible] = React.useState(false);
+  const [isLoginFormVisible, setIsLoginFormVisible] = useState(false);
+
   const toggleLoginForm = () => {
     setIsLoginFormVisible(!isLoginFormVisible);
   };
 
   return (
-    // Add html below
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item"><Link to="/">Home</Link></li>
-        <li className="navbar-item"><Link to="/dashboard">Dashboard</Link></li>
-        <li className="navbar-item"><Link to="/currency-converter">Currency Converter</Link></li>
-        <li className="navbar-item"><Link to="/expense-tracker">Expense Tracker</Link></li>
-        <li className="navbar-item"><Link to="/budget-forecasting">Budget Forecasting</Link></li>
-        <li className="navbar-item"><button onClick={toggleLoginForm} id='login-button'>Login</button></li>
-      </ul>
-      {isLoginFormVisible && (<LoginForm />)}
-    </nav>
-    // Add html above
+<nav className="navbar">
+  <ul className="navbar-list">
+    <li className="navbar-item"><Link to="/">Home</Link></li>
+    <li className="navbar-item"><Link to="/dashboard">Dashboard</Link></li>
+    <li className="navbar-item"><Link to="/currency-converter">Currency Converter</Link></li>
+    <li className="navbar-item"><Link to="/expense-tracker">Expense Tracker</Link></li>
+    <li className="navbar-item"><Link to="/budget-forecasting">Budget Forecasting</Link></li>
+    <li className="navbar-item"><DarkModeToggle /></li>
+    <li className="navbar-item"><button onClick={toggleLoginForm} id='login-button'>Login</button></li>
+  </ul>
+  {isLoginFormVisible && (<LoginForm />)}
+</nav>
   );
 };
 
