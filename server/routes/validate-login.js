101f54866419a25066914a9f69fdf9ab0cc868db
EllisOllier <ellisollier1@gmail.com>
2025-02-14 15:04:53 +0000

    Fixed api error 404

--- server/routes/validate-login.js
+++ server/routes/validate-login.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {validateLogin} = require("../controllers/validate-login");
+const { validateLogin } = require("../controllers/validate-login");
 
-router.post('/validateLogin', validateLogin);
+router.post('/validate-login', validateLogin);
 router.use((req, res) => {
     res.status(404).json({ error: 'Not Found' });
 });
-
 
 module.exports = router;
