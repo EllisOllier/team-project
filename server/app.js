9ddff921b5cf3eca490dd3321e83d820b48c1d2a
EllisOllier <ellisollier1@gmail.com>
2025-02-26 09:49:36 +0000

    Changed Track.js name to ExpenseTracker.js

--- server/app.js
+++ server/app.js
@@ -25,6 +25,9 @@ app.use("/api/status-check", checkApi);
 const addExpense = require("./routes/add-expense");
 app.use("/api/expenses/add", addExpense);
 
+const myApi = require();
+app.use("/api/expenses/get/", myApi);
+
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json()); // Middleware to parse JSON request bodies
 
