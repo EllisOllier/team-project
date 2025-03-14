1c05b844a2a774182299f64dc7f481016f21803f
EllisOllier <ellisollier1@gmail.com>
2025-03-14 16:20:32 +0000

    Updated login form and auth context to remember the user is logged in on page refresh

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,9 +1,17 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is logged in when the component mounts
+    const userID = localStorage.getItem('userID');
+    if (userID) {
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const logout = () => {
     setIsLoggedIn(false);
